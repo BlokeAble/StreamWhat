cf5d8e953e61187358c312a311979ef6a9ea150f
BlokeAble <bcaballero54@gmail.com>
2021-10-14 23:31:43 -0600

    Template for moive card

---
+++ FrontEnd/src/Components/Display/Display.js
@@ -0,0 +1,75 @@
+import React, { Component } from "react";
+import "./Display.css";
+
+class Found extends Component {
+
+  render() {
+    return (
+      <div className="movie_card">
+        <div className="info_section">
+          <div className="movie_header">
+            <img
+              //Moive Poster
+              className="locandina"
+              src= "https://image.tmdb.org/t/p/w780/zHzBTd502IZf60x8n2g0TB01vPF.jpg"
+              alt="Failed"
+            />
+            <h1> Hush </h1>
+            <h4>
+            2016, Mike Flanagan
+            </h4>
+            <span className="minutes"> 82 minutes</span>
+            <span className="streamService"> Netflix </span>
+          </div>
+          <div className="movie_desc">
+            <p className="text"> A deaf woman is stalked by a psychotic killer in her secluded home.</p>
+          </div>
+          <div class="movie_social">
+            <ul>
+              <li>
+                <a
+                  href= "https://www.netflix.com/title/80091879/"
+                  rel="noreferrer"
+                  target="_blank"
+                  class="gg-screen-wide"
+                ></a>
+              </li>
+              <li>
+                <a
+                  href="https://www.youtube.com/embed/Q_P8WCbhC6s"
+                  rel="noreferrer"
+                  target="_blank"
+                  class="gg-youtube"
+                ></a>
+              </li>
+              <li>
+                <a
+                  href= "https://www.imdb.com/title/tt5022702" 
+                  rel="noreferrer"
+                  target="_blank"
+                >
+                  IMDb
+                </a>
+                " 0%"
+              </li>
+              <li>
+                <a
+                  href= "https://www.themoviedb.org/movie/376570" 
+                  rel="noreferrer"
+                  target="_blank"
+                >
+                  TMDB
+                </a>
+               " 14%"
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="blur_back">
+          <img src="https://image.tmdb.org/t/p/w1280/57zBhcUGeeEunpEdqjfywK81ogd.jpg" alt="Failed" />
+        </div>
+      </div>
+    );
+  }
+}
+export default Found;
--- FrontEnd/src/Components/Found/Found.js
+++ FrontEnd/src/Components/Found/Found.js
@@ -1,20 +1,32 @@
 import React, { Component } from "react";
+import Display from "../Display/Display";
 import "./Found.css";
-
 
 class Found extends Component {
   state = {
     data: this.props.data,
     service: Object.keys(this.props.data.streamingInfo)[0],
-    region: Object.keys(this.props.data.streamingInfo[Object.keys(this.props.data.streamingInfo)[0]])[0]
+    region: Object.keys(
+      this.props.data.streamingInfo[
+        Object.keys(this.props.data.streamingInfo)[0]
+      ]
+    )[0],
   };
   componentDidUpdate(prevProp) {
     if (this.props.data !== prevProp.data) {
       this.setState({
-         data: this.props.data,
-         service: Object.keys(this.props.data.streamingInfo)[0],
-         region: Object.keys(this.props.data.streamingInfo[Object.keys(this.props.data.streamingInfo)[0]])[0]
-        });
+        data: this.props.data,
+        service: Object.keys(this.props.data.streamingInfo)[0],
+        region: Object.keys(
+          this.props.data.streamingInfo[
+            Object.keys(this.props.data.streamingInfo)[0]
+          ]
+        )[0],
+      });
+    }
+    else
+    {
+      <Display/>
     }
   }
   render() {
@@ -38,29 +50,51 @@ class Found extends Component {
           <div className="movie_desc">
             <p className="text">{this.state.data.overview}</p>
           </div>
-          <div class="movie_rating">
-            <ul>
-              <li>
-                  {this.state.data.imdbRating}
-              </li>
-              <li>
-                  {this.state.data.tmdbRating}
-              </li>
-            </ul>
-          </div>
           <div class="movie_social">
             <ul>
               <li>
-              <a  href={this.state.data.streamingInfo[this.state.service][this.state.region].link} rel="noreferrer"  target="_blank" class="gg-clapper-board"></a>
+                <a
+                  href={
+                    this.state.data.streamingInfo[this.state.service][
+                      this.state.region
+                    ].link
+                  }
+                  rel="noreferrer"
+                  target="_blank"
+                  class="gg-screen-wide"
+                ></a>
               </li>
               <li>
-                <a href={"https://www.youtube.com/embed/"+this.state.data.video}  rel="noreferrer" target="_blank" class="gg-youtube"></a>
+                <a
+                  href={
+                    "https://www.youtube.com/embed/" + this.state.data.video
+                  }
+                  rel="noreferrer"
+                  target="_blank"
+                  class="gg-youtube"
+                ></a>
               </li>
               <li>
-                <a href={"https://www.imdb.com/title/" +this.state.data.imdbID} rel="noreferrer" target="_blank" >IMDb</a>
+                <a
+                  href={"https://www.imdb.com/title/" + this.state.data.imdbID}
+                  rel="noreferrer"
+                  target="_blank"
+                >
+                  IMDb
+                </a>
+                {"  "+this.state.data.imdbRating +"%"}
               </li>
               <li>
-              <a href={"https://www.themoviedb.org/movie/"+this.state.data.tmdbID} rel="noreferrer" target="_blank" >TMDB</a>
+                <a
+                  href={
+                    "https://www.themoviedb.org/movie/" + this.state.data.tmdbID
+                  }
+                  rel="noreferrer"
+                  target="_blank"
+                >
+                  TMDB
+                </a>
+                {"  "+this.state.data.tmdbRating +"%"}
               </li>
             </ul>
           </div>
